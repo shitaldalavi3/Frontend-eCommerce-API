4418ad31793120376208c41691efe3234501677e
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-08 21:08:29 +0200

    no changes

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -11,7 +11,6 @@ const CartPage = () => {
         <thead>
           <tr>
             <th>Product</th>
-            <th>Quantity</th>
             <th>Price</th>
             <th>Total</th>
             <th>Actions</th>
